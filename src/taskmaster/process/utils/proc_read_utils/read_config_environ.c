2d044205b2041b8ff299481e2309007975967522
unicornslayer <terman.emil@gmail.com>
2018-01-24 19:49:46 +0200

    fixed the environment formating

--- src/taskmaster/process/utils/proc_read_utils/read_config_environ.c
+++ src/taskmaster/process/utils/proc_read_utils/read_config_environ.c
@@ -5,15 +5,28 @@ t_str	*read_config_environ(t_str val)
 {
 	t_str		*fields;
 	int			i;
+	int			ret;
+	regex_t		preg;
+
 
 	fields = ft_strsplit(val, ',');
+	if ((ret = regcomp(&preg, "^[a-zA-Z0-9_]+=.*$", REG_EXTENDED)) != 0)
+		regex_proj_error(&preg, ret, 0);
+
 	for (i = 0; fields[i]; i++)
-		if (ft_strchr(fields[i], '=') == NULL)
+	{
+		ret = regexec(&preg, fields[i], 0, NULL, 0);
+		if (ret == REG_NOMATCH)
 		{
-			ft_prerror(FALSE, "environment: Invalid environment format: %s\n",
-				fields[i]);
+			TASKMAST_ERROR(FALSE, "environment: Invalid "
+				"format: %s\n", fields[i]);
 			ft_free_bidimens(fields);
-			return NULL;
+			fields = NULL;
+			break;
 		}
+		else if (ret != 0)
+			regex_proj_error(&preg, ret, 1);
+	}
+	regfree(&preg);
 	return fields;
 }
